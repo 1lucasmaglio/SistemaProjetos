b28c9ba65498ba975d3df340dadc84de86fb8d5e
Lucas Maglio Chiabai <lucasmaglionovo@gmail.com>
2026-09-13 14:04:52 -0300

    Update Main.java

--- SistemaProjetos/src/Main.java
+++ SistemaProjetos/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("==============================");
+        System.out.println("      SISTEMA DE PROJETOS     ");
+        System.out.println("==============================");
+        System.out.println();
+        System.out.println("Projeto: Portfólio Acadêmico");
+        System.out.println("Desenvolvido em Java");
+        System.out.println("Versão: 1.0");
+        System.out.println();
+
         Projeto projeto1 = new Projeto(
             1,
             "Sistema Acadêmico",
