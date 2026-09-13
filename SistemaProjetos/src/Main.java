593ae0de75bd176d706172254d43af2501e12a97
Lucas Maglio Chiabai <lucasmaglionovo@gmail.com>
2026-09-13 13:30:50 -0300

    Update Main.java

--- SistemaProjetos/src/Main.java
+++ SistemaProjetos/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("==============================");
+        System.out.println("      SISTEMA DE PROJETOS     ");
+        System.out.println("==============================");
+        System.out.println();
+        System.out.println("Projeto: Portfólio Acadêmico");
+        System.out.println("Desenvolvido em Java");
+        System.out.println("Versão: 1.0");
+        System.out.println();
+
         Projeto projeto1 = new Projeto(
             1,
             "Sistema Acadêmico",
@@ -14,22 +23,14 @@ public class Main {
 
         Projeto projeto2 = new Projeto(
             2,
-            "Site Institucional",
-            "Website de uma instituição",
+            "Website Institucional",
+            "Um site para a Fatec",
             "Web",
             "Concluído"
         );
 
-        System.out.println("PROJETO 1");
-        System.out.println(projeto1.getNome());
-        System.out.println(projeto1.getCategoria());
-        System.out.println(projeto1.getStatus());
-
+        projeto1.exibirDados();
         System.out.println();
-
-        System.out.println("PROJETO 2");
-        System.out.println(projeto2.getNome());
-        System.out.println(projeto2.getCategoria());
-        System.out.println(projeto2.getStatus());
+        projeto2.exibirDados();
     }
 }
