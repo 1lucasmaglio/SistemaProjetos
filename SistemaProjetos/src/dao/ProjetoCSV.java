241f45232ea9af271b323479e6873ce98d08a6a1
Lucas Maglio Chiabai <lucasmaglionovo@gmail.com>
2026-09-23 17:29:02 -0300

    Update ProjetoCSV.java

--- SistemaProjetos/src/dao/ProjetoCSV.java
+++ SistemaProjetos/src/dao/ProjetoCSV.java
@@ -1,32 +1,31 @@
 package dao;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Projeto;
 
 public class ProjetoCSV {
+
     private Path caminho;
-    
+
     public ProjetoCSV() {
-        caminho = Paths.get("dao/projetos.csv");
+        caminho = Path.of("dados/projetos.csv");
     }
-    
-    public void salvar(List<Projeto> projetos) throws IOException {
+
+    public void salvar(List<Projeto> projetos) throws Exception {
         List<String> linhas = new ArrayList<>();
+
         linhas.add("id;nome;descricao;categoria;status");
 
         for (Projeto projeto : projetos) {
-            String linha =
-            projeto.getId() + ";" +
-            projeto.getNome() + ";" +
-            projeto.getDescricao() + ";" +
-            projeto.getCategoria() + ";" +
-            projeto.getStatus();
+            String linha = projeto.getId() + ";" +
+                    projeto.getNome() + ";" +
+                    projeto.getDescricao() + ";" +
+                    projeto.getCategoria() + ";" +
+                    projeto.getStatus();
 
             linhas.add(linha);
         }
@@ -34,21 +33,18 @@ public class ProjetoCSV {
         Files.write(caminho, linhas);
     }
 
-    public List<Projeto> listar() throws IOException {
+    public List<Projeto> listar() throws Exception {
         List<Projeto> projetos = new ArrayList<>();
 
         if (!Files.exists(caminho)) {
             return projetos;
         }
 
-        List<String> linhas =
-            Files.readAllLines(caminho);
+        List<String> linhas = Files.readAllLines(caminho);
 
-        for(int i = 1; i < linhas.size(); i++) {
+        for (int i = 1; i < linhas.size(); i++) {
             String linha = linhas.get(i);
-
-            String[] dados = 
-                linha.split(";");
+            String[] dados = linha.split(";");
 
             int id = Integer.parseInt(dados[0]);
             String nome = dados[1];
@@ -56,14 +52,7 @@ public class ProjetoCSV {
             String categoria = dados[3];
             String status = dados[4];
 
-            Projeto projeto = new Projeto(
-                id,
-                nome,
-                descricao,
-                categoria,
-                status
-            );
-
+            Projeto projeto = new Projeto(id, nome, descricao, categoria, status);
             projetos.add(projeto);
         }
 
